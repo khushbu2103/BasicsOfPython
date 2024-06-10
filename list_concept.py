97d7789c7425448bf2f54d09d7b942887f5a6452
khushbu <khushbusoni475@gmail.com>
2024-06-10 15:40:45 +0530

    [khushbu] Add : python script to implement list concept

---
+++ list_concept.py
@@ -0,0 +1,13 @@
+# list = []
+# movie1 = input("enter movie 1")
+# movie2 = input("enter movie 2")
+# movie3 = input("enter movie 3")
+# list.append(movie1)
+# list.append(movie2)
+# list.append(movie3)
+# print(list)
+
+list2 = []
+film1 = list2.append(input("enter film 1"))
+film2 = list2.append(input("enter film 2"))
+print(list2)
